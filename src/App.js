5b802a004303404f3483c73c86cf6687c9c403a5
Sathi Guchait <sathiguchait8@gmail.com>
2023-12-27 14:15:05 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 // import { useFormik } from 'formik';
 // import * as Yup from 'yup';
 
@@ -7,18 +8,19 @@ import './App.css';
 
 function App() {
 
-  const [formData, setFormData] = useState({firstName: '',lastName: '',email: '', phone: ''});
-  
-  const [formErrors, setFormErrors] = useState({firstName: '',lastName: '',email: '',phone:''});
+
+  const [formData, setFormData] = useState({ firstName: '', lastName: '', email: '', phone: '' });
+  const [formErrors, setFormErrors] = useState({ firstName: '', lastName: '', email: '', phone: '' });
+  const initialFormData = { firstName: '', lastName: '', email: '', phone: '' };
+  const [successMessage, setSuccessMessage] = useState('');
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData({...formData,[name]: value,});
+    setFormData({ ...formData, [name]: value, });
 
-    setFormErrors({...formErrors,[name]: '',});
+    setFormErrors({ ...formErrors, [name]: '', });
 
   };
-
   const validateForm = () => {
     let valid = true;
     const newFormErrors = {};
@@ -35,10 +37,10 @@ function App() {
       valid = false;
     }
     // validate phone number
-    const phoneRegex = /^[0-9]{10}$/ ;
-    if(!formData.phone.trim() || !phoneRegex.test(formData.phone)) {
+    const phoneRegex = /^[0-9]{10}$/;
+    if (!formData.phone.trim() || !phoneRegex.test(formData.phone)) {
       newFormErrors.phone = 'valid phone number is required';
-      valid=false;
+      valid = false;
     }
 
 
@@ -53,88 +55,98 @@ function App() {
     return valid;
   };
 
-
-
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-     if (validateForm()) {
-     
-      console.log('Form submitted:', formData);
-    } else {
-    
-      console.error('Form validation failed');
-    }
-  };
-// const formik = useFormik({
-//    initialValues:{
-//     firstName: '',
-//     lastName: '',
-//     phone: '',
-//     email: ''
-//    },
-//    validationSchema: Yup.object({
-//     firstName: Yup.string().required(),
-//     lastName: Yup.string().required(),
-//     phone: Yup.string().required().min(10),
-//     email: Yup.string().required().email()
-   
-//    }),
-//    onsubmit:(data)=>{
-//     console.log("formik data", data)
-//    }
+    if (validateForm()) {
+      try {
+        // Send form data using Axios
+        const response = await axios.post('https://jsonplaceholder.typicode.com/posts', formData);
+
+        if (response.status === 201) {
+          console.log(response);
+          setFormData(initialFormData);
+          setSuccessMessage('Form submitted successfully!');
+          setTimeout(() => setSuccessMessage(''), 4000);
+        } else {
+          console.error('Form submission failed.');
+        }
+      } catch (error) {
+        console.error('Error submitting form:', error);
+      }
+    };
+  }
+  // const formik = useFormik({
+  //    initialValues:{
+  //     firstName: '',
+  //     lastName: '',
+  //     phone: '',
+  //     email: ''
+  //    },
+  //    validationSchema: Yup.object({
+  //     firstName: Yup.string().required(),
+  //     lastName: Yup.string().required(),
+  //     phone: Yup.string().required().min(10),
+  //     email: Yup.string().required().email()
+
+  //    }),
+  //    onsubmit:(data)=>{
+  //     console.log("formik data", data)
+  //    }
 
 
-// })
+  // })
 
 
   return (
     <div className="App">
-    <h1>Simple Form</h1>
-    <div className='container'>
-    <form method="post" onSubmit={handleSubmit}>
+      {successMessage && <div className='success'>{successMessage}</div>}
+     
+      <h1>Simple Form</h1>
+      <div className='container'>
+        <form onSubmit={handleSubmit} >
 
-        <input
-          type="text"
-          name="firstName"
-          value={formData.firstName}
-          onChange={handleChange}
-          placeholder='first name'
-        />
+          <input
+            type="text"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleChange}
+            placeholder='first name'
+          />
           <div className="error">{formErrors.firstName}</div>
-      <br />
-        <input
-          type="text"
-          name="lastName"
-          value={formData.lastName}
-          onChange={handleChange}
-          placeholder='last name'
-        />
-         <div className="error">{formErrors.lastName}</div>
-      <br />
-      <input
-          type="phone"
-          name="phone"
-          value={formData.phone}
-          onChange={handleChange}
-          placeholder='phone number'
-        />
-         <div className="error">{formErrors.phone}</div>
-        <br />
-        <input
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          placeholder='email'
-        />
-     <div className="error">{formErrors.email}</div>
-      <br />
+          <br />
+          <input
+            type="text"
+            name="lastName"
+            value={formData.lastName}
+            onChange={handleChange}
+            placeholder='last name'
+          />
+          <div className="error">{formErrors.lastName}</div>
+          <br />
+          <input
+            type="phone"
+            name="phone"
+            value={formData.phone}
+            onChange={handleChange}
+            placeholder='phone number'
+          />
+          <div className="error">{formErrors.phone}</div>
+          <br />
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            placeholder='email'
+          />
+          <div className="error">{formErrors.email}</div>
+          <br />
 
-      <button type="submit">Submit</button>
-    </form>
+          <button type="submit" >Submit</button>
+        </form>
+      </div>
     </div>
-  </div>
   );
 }
 
